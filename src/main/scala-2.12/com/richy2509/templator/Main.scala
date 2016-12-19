e68a9cdd937a13d8c15e967fc40dad874d3a7a3a
richard koehl <koehl.richard@gmail.com>
2016-12-19 08:48:33 +0100

    Add files

---
+++ src/main/scala-2.12/com/richy2509/templator/Main.scala
@@ -0,0 +1,15 @@
+package com.richy2509.templator
+
+/**
+  * Created by richardkoehl on 19/12/2016.
+  */
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+    println("bonjour")
+
+  }
+
+
+}
--- src/test/scala/com/richy2509.templator/JsonParserTest.scala
+++ src/test/scala/com/richy2509.templator/JsonParserTest.scala
@@ -1,10 +1,8 @@
 package com.richy2509.templator
 
-import java.io.FileInputStream
-
 import com.typesafe.scalalogging.Logger
 import org.scalatest._
-import play.api.libs.json.{Json, __, _}
+import play.api.libs.json._
 
 class JsonParserTest extends FlatSpec with Matchers {
 
@@ -13,55 +11,43 @@ class JsonParserTest extends FlatSpec with Matchers {
   "A json" should "retrieve value" in {
     val stream = getClass.getResourceAsStream("/test.json")
 
-    val j = Json.parse(
-      """{
- "key1" : "value1",
- "key2" : {
-   "key21" : 123,
-   "key22" : true,
-   "key23" : [ "alpha", "beta", "gamma"],
-   "key24" : {
-      "key241" : 234.123,
-      "key242" : "/data.json"
-    }
- },
- "key3" : 234
- }""".stripMargin)
+    val includeControllerJ =
+      """
+        |{
+        | "adminController": {}
+        |,"bookingController": {}
+        |,"homeController": {}
+        |}
+      """.stripMargin
 
-    import play.api.libs.json._
+    val includeDataJ =
+      """
+        |{
+        | "adminController": {}
+        |,"bookingController": {}
+        |,"homeController": {}
+        |}
+      """.stripMargin
 
-    val path = __ \ 'key2 \ 'key24
+    val mainJ = Json.parse(
+      """
+      |{
+      |  "include": ["controller.json", "data.json"]
+      |}
+      """.stripMargin)
 
-    val filepath = (j \ "key2" \ "key24" \ "key242").as[String]
+
+    val path = __
+
+    val filepath = (mainJ \ "include").as[Array[String]]
 
     val jsonTransformer = path.json.update(
-      __.read[JsObject].map{ o => o ++ Json.obj( "key242" -> Json.parse(getClass.getResourceAsStream(filepath)) ) }
+      __.read[JsObject].map { o => o ++ Json.obj("include" ->
+        Json.parse(filepath(0))
+      ) }
     )
 
-    println(Json.prettyPrint(j.transform(jsonTransformer).get))
-
-    /*
-
-    val content = "bonjour"
-
-    val list = List("n", "j", "r")
-
-    val newC = list.foldLeft(content) {
-      (newContent, item) => {
-        logger.debug(newContent)
-        s"$newContent;${content.indexOf(item)}"
-      }
-    }
-
-    println(s"[finalContent] $newC")
-
-    val newRoot = fields.foldLeft(root){
-      (acc, r) => acc
-    }
-
-    println(Json.prettyPrint(newRoot))
-
-    root shouldNot be(null)*/
+    println(Json.prettyPrint(mainJ.transform(jsonTransformer).get))
 
   }
 
